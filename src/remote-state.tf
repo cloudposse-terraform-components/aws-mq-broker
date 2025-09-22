b5734bfa1bb02c6c0cf569c0272622a2bbb7ea18
Igor Rodionov <goruha@gmail.com>
2025-09-22 19:36:18 +0200

    Refactor remote-state module configuration (#38)

--- src/remote-state.tf
+++ src/remote-state.tf
@@ -2,18 +2,14 @@ module "vpc" {
   source  = "cloudposse/stack-config/yaml//modules/remote-state"
   version = "1.8.0"
 
-  stack_config_local_path = "../../../stacks"
-  component               = "vpc"
-
-  context = module.this.context
+  component = "vpc"
+  context   = module.this.context
 }
 
 module "eks" {
   source  = "cloudposse/stack-config/yaml//modules/remote-state"
   version = "1.8.0"
 
-  stack_config_local_path = "../../../stacks"
-  component               = "eks"
-
-  context = module.this.context
+  component = "eks"
+  context   = module.this.context
 }
