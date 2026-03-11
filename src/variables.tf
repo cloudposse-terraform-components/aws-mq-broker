b3c905e8dd5a399b9769e06ee125319cc233a9c8
RoseSecurity <michael@rosesecurity.dev>
2026-03-11 15:42:31 -0400

    fix: Remove deprecated Terraform syntax
    Automated fix using tflint --fix to resolve deprecated HCL syntax:
    
    - terraform_deprecated_interpolation: Replace "${var.x}" with var.x
    - terraform_deprecated_index: Replace .0 with [0]
    - terraform_deprecated_lookup: Replace deprecated lookup() usage

--- src/variables.tf
+++ src/variables.tf
@@ -57,76 +57,16 @@ variable "audit_log_enabled" {
   description = "Enables audit logging. User management action made using JMX or the ActiveMQ Web Console is logged"
 }
 
-variable "maintenance_day_of_week" {
-  type        = string
-  default     = "SUNDAY"
-  description = "The maintenance day of the week. e.g. MONDAY, TUESDAY, or WEDNESDAY"
-}
-
-variable "maintenance_time_of_day" {
-  type        = string
-  default     = "03:00"
-  description = "The maintenance time, in 24-hour format. e.g. 02:00"
-}
-
-variable "maintenance_time_zone" {
-  type        = string
-  default     = "UTC"
-  description = "The maintenance time zone, in either the Country/City format, or the UTC offset format. e.g. CET"
-}
-
-variable "mq_admin_user" {
-  type        = string
-  default     = null
-  description = "Admin username"
-}
-
-variable "mq_admin_password" {
-  type        = string
-  default     = null
-  description = "Admin password"
-}
-
-variable "mq_application_user" {
-  type        = string
-  default     = null
-  description = "Application username"
-}
-
-variable "mq_application_password" {
-  type        = string
-  default     = null
-  description = "Application password"
-}
-
 variable "allowed_security_groups" {
   type        = list(string)
   default     = []
   description = "List of security groups to be allowed to connect to the broker instance"
 }
 
-variable "allowed_cidr_blocks" {
-  type        = list(string)
-  default     = []
-  description = "List of CIDR blocks that are allowed ingress to the broker's Security Group created in the module"
-}
-
-variable "overwrite_ssm_parameter" {
-  type        = bool
-  default     = true
-  description = "Whether to overwrite an existing SSM parameter"
-}
-
 variable "use_existing_security_groups" {
   type        = bool
   default     = false
   description = "Flag to enable/disable creation of Security Group in the module. Set to `true` to disable Security Group creation and provide a list of existing security Group IDs in `existing_security_groups` to place the broker into"
-}
-
-variable "existing_security_groups" {
-  type        = list(string)
-  default     = []
-  description = "List of existing Security Group IDs to place the broker into. Set `use_existing_security_groups` to `true` to enable using `existing_security_groups` as Security Groups for the broker"
 }
 
 variable "ssm_parameter_name_format" {
